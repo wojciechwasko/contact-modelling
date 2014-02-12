5246145d60154e3ccb773bb28aae8bddd04198bf
Wojciech Waśko <wojciech.wasko@gmail.com>
2014-02-12 21:45:19 +0100

    Expanded documentation on MeshNode.

--- client/inc/MeshNode.hpp
+++ client/inc/MeshNode.hpp
@@ -4,7 +4,17 @@
 #include <cstddef>
 
 /**
- * \brief   A "POD" struct to keep x,y coordinates of a node in the mesh; with 1D values (e.g. only normal forces).
+ * \brief   A "POD" struct to keep x,y coordinates of a node in the mesh; with
+ *          D values.
+ * \tparam  D   dimensionality of the values stored in the node.
+ * \note    The values are not stored here directly; rather, a pointer to the
+ *          value will be stored; the values are stored on Mesh'es side (usually 
+ *          in a vector/array which will ensure continuity of memory).
+ *
+ * As an example, let's consider a 3D value (e.g. 3D forces)
+ * vals[0] - Fx
+ * vals[1] - Fy
+ * vals[2] - Fz
  */
 template <std::size_t D>
 struct MeshNode {
