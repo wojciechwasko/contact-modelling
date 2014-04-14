6905459c4f4929e95da054069cd342bb233c1f6f
Wojciech Waśko <wojciech.wasko@gmail.com>
2014-04-15 01:01:44 +0200

    !important flipping signs in the forces-to-displacements matrix #254
    I discovered that the effective displacement was defined as u(z=0) - u(z=h), which would all be good
    if z = 0 corresponded to the skin surface and z = h to the substrate surface, but it was the other
    way around.
    
    Also: New generator for unit testing the elastic model, written in python

---
+++ contact-modelling/test/gen/gen.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+node1 = (0.1, 0.1)
+node2_diff = (0.105, 0.107)
+node2_same = (0.1, 0.1)
+h = 0.002
+G = 300000
+
+dx_diff = node2_diff[0]-node1[0]
+dy_diff = node2_diff[1]-node1[1]
+
+dx_same = node2_same[0]-node1[0]
+dy_same = node2_same[1]-node1[1]
+
+xx_diff = lambda dx,dy,h,pi,G : 3 * (-(2*dx**2 + dy**2)/((dx**2 + dy**2)**1.5) + (2*dx**2 + dy**2 + h**2)/((dx**2 + dy**2 + h**2)**1.5))/(4*pi*G)
+yx_diff = lambda dx,dy,h,pi,G : 3 * (-(dx * dy) / ((dx**2 + dy**2)**1.5) + (dx * dy) / ((dx**2 + dy**2 + h**2)**1.5)) / (4*pi*G)
+zx_diff = lambda dx,dy,h,pi,G : 3 * ((dx * h) / ((dx**2 + dy**2 + h**2)**1.5)) / (4*pi*G)
+xy_diff = lambda dx,dy,h,pi,G : 3 * (-(dx*dy)/((dx**2+dy**2)**1.5) + (dx*dy)/((dx**2+dy**2+h**2)**1.5) ) / (4*pi*G)
+yy_diff = lambda dx,dy,h,pi,G : 3 * (-(dx**2+2*dy**2)/((dx**2+dy**2)**1.5) + (dx**2 + 2*dy**2 + h**2)/((dx**2 + dy**2 + h**2)**1.5)) /(4*pi*G)
+zy_diff = lambda dx,dy,h,pi,G : 3 * ((dy*h)/((dx**2 + dy**2 + h**2)**1.5)) / (4*pi*G)
+xz_diff = lambda dx,dy,h,pi,G : 3 * ((dx * h) / ((dx**2 + dy**2 + h**2)**1.5)) / (4*pi*G)
+yz_diff = lambda dx,dy,h,pi,G : 3 * ((dy * h) / ((dx**2 + dy**2 + h**2)**1.5)) / (4*pi*G)
+zz_diff = lambda dx,dy,h,pi,G : 3 * (-1/((dx**2+dy**2)**0.5) + (dx**2+dy**2+2*h**2)/((dx**2+dy**2+h**2)**1.5)) / (4*pi*G)
+
+xx_same = lambda pi,G : 9/(4*pi*G) * 0.25/0.0309019361618552
+yx_same = lambda pi,G : 0
+zx_same = lambda pi,G : 0
+xy_same = lambda pi,G : 0
+yy_same = lambda pi,G : 9/(4*pi*G) * 0.25/0.0309019361618552
+zy_same = lambda pi,G : 0
+xz_same = lambda pi,G : 0
+yz_same = lambda pi,G : 0
+zz_same = lambda pi,G : 9/(2*pi*G) * 0.25/0.0309019361618552
+
+vec_head = "std::vector<double> expected_%s = {"
+vec_tail = "};"
+
+import math
+print(
+vec_head % "diff_3_3" +
+"%s,%s,%s, %s,%s,%s, %s,%s,%s" % (
+  xx_diff(dx_diff, dy_diff, h, math.pi, G),
+  yx_diff(dx_diff, dy_diff, h, math.pi, G),
+  zx_diff(dx_diff, dy_diff, h, math.pi, G),
+  xy_diff(dx_diff, dy_diff, h, math.pi, G),
+  yy_diff(dx_diff, dy_diff, h, math.pi, G),
+  zy_diff(dx_diff, dy_diff, h, math.pi, G),
+  xz_diff(dx_diff, dy_diff, h, math.pi, G),
+  yz_diff(dx_diff, dy_diff, h, math.pi, G),
+  zz_diff(dx_diff, dy_diff, h, math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "diff_1_3"+
+"%s,%s,%s" % (
+  zx_diff(dx_diff, dy_diff, h, math.pi, G),
+  zy_diff(dx_diff, dy_diff, h, math.pi, G),
+  zz_diff(dx_diff, dy_diff, h, math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "diff_3_1" +
+"%s,%s,%s" % (
+  xz_diff(dx_diff, dy_diff, h, math.pi, G),
+  yz_diff(dx_diff, dy_diff, h, math.pi, G),
+  zz_diff(dx_diff, dy_diff, h, math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "diff_1_1" +
+"%s" % (
+  zz_diff(dx_diff, dy_diff, h, math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "same_3_3" +
+"%s,%s,%s, %s,%s,%s, %s,%s,%s" % (
+  xx_same(math.pi, G),
+  yx_same(math.pi, G),
+  zx_same(math.pi, G),
+  xy_same(math.pi, G),
+  yy_same(math.pi, G),
+  zy_same(math.pi, G),
+  xz_same(math.pi, G),
+  yz_same(math.pi, G),
+  zz_same(math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "same_1_3" +
+"%s,%s,%s" % (
+  zx_same(math.pi, G),
+  zy_same(math.pi, G),
+  zz_same(math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "same_3_1" +
+"%s,%s,%s" % (
+  xz_same(math.pi, G),
+  yz_same(math.pi, G),
+  zz_same(math.pi, G),
+  )
++ vec_tail)
+
+print(
+vec_head % "same_1_1" +
+"%s" % (
+  zz_same(math.pi, G),
+  )
++ vec_tail)
+
--- contact-modelling/test/test_elastic_model.cpp
+++ contact-modelling/test/test_elastic_model.cpp
@@ -38,6 +38,16 @@ private:
   }
 };
 
+// generated with gen/gen.py
+std::vector<double> expected_diff_3_3 = {-4.77612499182e-06,-3.32211154015e-06,1.15517619829e-05, -3.32211154015e-06,-7.05414433363e-06,1.61724667761e-05, 1.15517619829e-05,1.61724667761e-05,2.21751661574e-06};
+std::vector<double> expected_diff_1_3 = {1.15517619829e-05,1.61724667761e-05,2.21751661574e-06};
+std::vector<double> expected_diff_3_1 = {1.15517619829e-05,1.61724667761e-05,2.21751661574e-06};
+std::vector<double> expected_diff_1_1 = {2.21751661574e-06};
+std::vector<double> expected_same_3_3 = {1.93137101012e-05,0,0, 0,1.93137101012e-05,0, 0,0,3.86274202023e-05};
+std::vector<double> expected_same_1_3 = {0,0,3.86274202023e-05};
+std::vector<double> expected_same_3_1 = {0,0,3.86274202023e-05};
+std::vector<double> expected_same_1_1 = {3.86274202023e-05};
+
 // -- TEST CASES: mesh nodes "far away", i.e. dx > eps_samepoint && dy > eps_samepoint
 // !begin
 
@@ -46,27 +56,15 @@ BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_3_3)
   MockMesh forces_mesh(3, 1);
   MockMesh disps_mesh(3, 1);
 
-  // note: for generator dx = 0.0005, dy = 0.0007 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1005,0.1007});
+  disps_mesh.add_node({0.105,0.107});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.000284267757585335,
-    0.000136847848007961,
-    -2.57040388378672e-05,
-    0.000136847848007961,
-    0.000378106281933651,
-    -3.59856543730141e-05,
-    -2.57040388378672e-05,
-    -3.59856543730141e-05,
-    8.37031393710367e-05
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_diff_3_3, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_1_3)
@@ -74,21 +72,15 @@ BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_1_3)
   MockMesh forces_mesh(3, 1);
   MockMesh disps_mesh(1, 1);
 
-  // note: for generator dx = 0.0002, dy = 0.0003 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1002,0.1003});
+  disps_mesh.add_node({0.105,0.107});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -1.2641650958631e-05,
-    -1.89624764379465e-05,
-    0.000478752428959904
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_diff_1_3, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_3_1)
@@ -96,21 +88,15 @@ BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_3_1)
   MockMesh forces_mesh(1, 1);
   MockMesh disps_mesh(3, 1);
 
-  // note: for generator dx = 0.0003, dy = 0.0004 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1003,0.1004});
+  disps_mesh.add_node({0.105,0.107});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -1.81650558119599e-05,
-    -2.42200744159466e-05,
-    0.000280746959558536
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_diff_3_1, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_1_1)
@@ -120,17 +106,14 @@ BOOST_AUTO_TEST_CASE(em_other_nodes_1_1_dim_1_1)
 
   // note: for generator dx = 0.0001, dy = 0.0004 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1001,0.1004});
+  disps_mesh.add_node({0.105,0.107});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.000388846272447214
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_diff_1_1, 1e-5);
 }
 // !end
 
@@ -145,26 +128,14 @@ BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_3_3)
 
   // note: for generator dx = 1e-5, dy = 1e-6 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-5, 0.1 + 1e-6});
+  disps_mesh.add_node({0.1, 0.1});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    1.93137101011595e-05,
-                       0,
-                       0,
-                       0,
-    1.93137101011595e-05,
-                       0,
-                       0,
-                       0,
-     3.8627420202319e-05
-
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_same_3_3, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_1_3)
@@ -174,19 +145,14 @@ BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_1_3)
 
   // note: for generator dx = 1e-5, dy = 1e-6 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-5, 0.1 + 1e-6});
+  disps_mesh.add_node({0.1, 0.1});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-                      0,
-                      0,
-    3.8627420202319e-05
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_same_1_3, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_3_1)
@@ -196,19 +162,14 @@ BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_3_1)
 
   // note: for generator dx = 1e-5, dy = 1e-6 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-5, 0.1 + 1e-6});
+  disps_mesh.add_node({0.1, 0.1});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-                      0,
-                      0,
-    3.8627420202319e-05
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_same_3_1, 1e-5);
 }
 
 BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_1_1)
@@ -218,204 +179,14 @@ BOOST_AUTO_TEST_CASE(em_same_nodes_1_1_dim_1_1)
 
   // note: for generator dx = 1e-5, dy = 1e-6 (it'd disps.x/y - forces.x/y)
   forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-5, 0.1 + 1e-6});
+  disps_mesh.add_node({0.1, 0.1});
 
   typedef AlgForcesToDisplacements alg_forces_to_disps;
   alg_forces_to_disps::params_type                    params_forces_to_disps;
   params_forces_to_disps.skin_props.E = 300000;
   params_forces_to_disps.skin_props.h  = 0.002;
   arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    3.8627420202319e-05
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
+  CHECK_CLOSE_COLLECTION(precomputed, expected_same_1_1, 1e-5);
 }
 // !end
 
-// -- TEST CASES: mesh nodes "very close" in one direction (i.e. dx > eps_samepoint xor dy > eps_samepoint)
-// !begin
-
-BOOST_AUTO_TEST_CASE(em_onedirx_nodes_1_1_dim_3_3)
-{
-  MockMesh forces_mesh(3, 1);
-  MockMesh disps_mesh(3, 1);
-
-  // note: for generator dx = 1e-6, dy = 0.0007 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-6, 0.1007});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.000253757610931623,
-    5.21825396597814e-07,
-    -5.57608288082757e-08,
-    5.21825396597814e-07,
-     0.00061903464308524,
-    -3.9032580165793e-05,
-    -5.57608288082757e-08,
-    -3.9032580165793e-05,
-    0.000142235207850219
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onediry_nodes_1_1_dim_3_3)
-{
-  MockMesh forces_mesh(3, 1);
-  MockMesh disps_mesh(3, 1);
-
-  // note: for generator dx = 0.0005, dy = 1e-6 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1, 0.1});
-  disps_mesh.add_node({0.1 + 0.0005, 0.1 + 1e-6});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.000924790809052427,
-    1.04588904661277e-06,
-    -3.02750823346094e-05,
-    1.04588904661277e-06,
-    0.000401848377524135,
-    -6.05501646692188e-08,
-    -3.02750823346094e-05,
-    -6.05501646692188e-08,
-    0.000280745956407604
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onedirx_nodes_1_1_dim_1_3)
-{
-  MockMesh forces_mesh(3, 1);
-  MockMesh disps_mesh(1, 1);
-
-  // note: for generator dx = 1e-6, dy = 0.0003 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-6, 0.1003});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -6.4137755190822e-08,
-    -1.92413265572466e-05,
-    0.000624751964323888
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onediry_nodes_1_1_dim_1_3)
-{
-  MockMesh forces_mesh(3, 1);
-  MockMesh disps_mesh(1, 1);
-
-  // note: for generator dx = 0.0002, dy = 1e-6 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1002, 0.1 + 1e-6});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -1.30664215002714e-05,
-    -6.53321075013572e-08,
-     0.00106363950928189
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onedirx_nodes_1_1_dim_3_1)
-{
-  MockMesh forces_mesh(1, 1);
-  MockMesh disps_mesh(3, 1);
-
-  // note: for generator dx = 1e-6, dy = 0.0004 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-6, 0.1004});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -6.25257233026628e-08,
-    -2.50102893210651e-05,
-    0.000408038541558231
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onediry_nodes_1_1_dim_3_1)
-{
-  MockMesh forces_mesh(1, 1);
-  MockMesh disps_mesh(3, 1);
-
-  // note: for generator dx = 0.0003, dy = 1e-6 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1003, 0.1 + 1e-6});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    -1.92413265572466e-05,
-    -6.4137755190822e-08,
-    0.000624751964323888
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onedirx_nodes_1_1_dim_1_1)
-{
-  MockMesh forces_mesh(1, 1);
-  MockMesh disps_mesh(1, 1);
-
-  // note: for generator dx = 1e-6, dy = 0.0004 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1 + 1e-6, 0.1004});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.000408038541558231
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-
-BOOST_AUTO_TEST_CASE(em_onediry_nodes_1_1_dim_1_1)
-{
-  MockMesh forces_mesh(1, 1);
-  MockMesh disps_mesh(1, 1);
-
-  // note: for generator dx = 0.0002, dy = 1e-6 (it'd disps.x/y - forces.x/y)
-  forces_mesh.add_node({0.1,0.1});
-  disps_mesh.add_node({0.1002, 0.1 + 1e-6});
-
-  typedef AlgForcesToDisplacements alg_forces_to_disps;
-  alg_forces_to_disps::params_type                    params_forces_to_disps;
-  params_forces_to_disps.skin_props.E = 300000;
-  params_forces_to_disps.skin_props.h  = 0.002;
-  arma::mat precomputed = boost::any_cast<arma::mat>(alg_forces_to_disps().offline(forces_mesh, disps_mesh, params_forces_to_disps));
-  std::vector<double> expected = {
-    0.00106363950928189
-  };
-  CHECK_CLOSE_COLLECTION(precomputed, expected, 1e-5);
-}
-// !end
