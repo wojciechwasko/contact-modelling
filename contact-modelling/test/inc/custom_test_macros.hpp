af65b45bdae25dd2be78a32364523b2b0e95aade
Wojciech Waśko <wojciech.wasko@gmail.com>
2014-04-15 14:04:36 +0200

    Working logging framework. Probably needs more docs, though. #253

--- contact-modelling/test/inc/custom_test_macros.hpp
+++ contact-modelling/test/inc/custom_test_macros.hpp
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_TEST_MACROS_HPP
 #define CUSTOM_TEST_MACROS_HPP
 
+#include <string>
+
 // rip-off from http://stackoverflow.com/a/17503436/312650
 // Have to make it a macro so that it reports exact line numbers when checks fail.
 #define CHECK_CLOSE_COLLECTION(aa, bb, tolerance) { \
@@ -14,6 +16,9 @@
         BOOST_CHECK_CLOSE(*a, *b, tolerance); \
     } \
 }
+
+#define CHECK_EQUAL_STR( s, t ) \
+  BOOST_CHECK_EQUAL(std::string(s), std::string(t));
 
 
 #endif /* CUSTOM_TEST_MACROS_HPP */
