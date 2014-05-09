caeb3e5cbfe2321a823f8e1abc58614e1dc91210
Wojciech Waśko <wojciech.wasko@gmail.com>
2014-05-09 17:39:21 +0200

    Introduced regular rectangular mesh; square mesh inherits from it

---
+++ contact-modelling/src/MeshRegularRectangular.cpp
@@ -0,0 +1,123 @@
+#include "MeshRegularRectangular.hpp"
+
+#include <stdexcept>
+#include <cmath>
+
+#include "helpers/string.hpp"
+
+using helpers::string::sb;
+
+MeshRegularRectangular::MeshRegularRectangular(const MeshInterface& base_mesh, const double dx, const double dy)
+  : MeshRegularRectangular(
+      base_mesh.D,
+      base_mesh.minX(), 
+      base_mesh.minY(),
+      base_mesh.maxX(),
+      base_mesh.maxY(),
+      dx,
+      dy
+    )
+{
+}
+
+MeshRegularRectangular::MeshRegularRectangular(const MeshInterface& base_mesh, const double dx, const double dy, const size_t dim)
+  : MeshRegularRectangular(
+      dim,
+      base_mesh.minX(), 
+      base_mesh.minY(),
+      base_mesh.maxX(),
+      base_mesh.maxY(),
+      dx,
+      dy
+    )
+{
+}
+
+MeshRegularRectangular::MeshRegularRectangular(
+  const size_t dim,
+  const double x0,
+  const double y0,
+  const double x1,
+  const double y1,
+  const double dx,
+  const double dy
+)
+  : MeshInterface(dim, calculate_no_nodes(x0, y0, x1, y1, dx, dy)), dx_(dx), dy_(dy)
+{
+  const size_t nx       = calculate_no_nodes_1D(x0, x1, dx);
+  const double diff_x   = nx * dx - (x1 - x0);
+  const double x_origin = x0 - diff_x / 2.;
+
+  const size_t ny       = calculate_no_nodes_1D(y0, y1, dy);
+  const double diff_y   = ny * dy - (y1 - y0);
+  const double y_origin = y0 - diff_y / 2.;
+
+  size_t in = 0;
+  double x = x_origin;
+  for (size_t ix = 0; ix < nx; ++ix) {
+    double y = y_origin;
+    for (size_t iy = 0; iy < ny; ++iy) {
+      this->node(in).x = x;
+      this->node(in).y = y;
+      y += dy;
+      ++in;
+    }
+    x += dx;
+  }
+  generateMinMax();
+}
+
+double 
+MeshRegularRectangular::impl_node_area(size_t i) const
+{
+  return dx_*dy_;
+}
+
+size_t 
+MeshRegularRectangular::calculate_no_nodes(double x0, double y0, double x1, double y1, double dx, double dy)
+{
+  if (!(x1 > x0)) {
+    throw std::runtime_error(
+      sb()  << "Error constructing regular square mesh, !(x1 > x0); "
+            << "x1 == " << x1 << "x0 == " << x0
+    );
+  }
+  if (!(y1 > y0)) {
+    throw std::runtime_error(
+      sb()  << "Error constructing regular square mesh, !(y1 > y0); "
+            << "y1 == " << y1 << "y0 == " << y0
+    );
+  }
+
+  return calculate_no_nodes_1D(x0, x1, dx) * calculate_no_nodes_1D(y0, y1, dy);
+}
+
+size_t 
+MeshRegularRectangular::calculate_no_nodes_1D(double t0, double t1, double d)
+{
+  if (!(t1 > t0)) {
+    throw std::runtime_error(
+      sb()  << "Error constructing regular square mesh, !(t1 > t0); "
+            << "t1 == " << t1 << "t0 == " << t0
+    );
+  }
+  if (!(d > 0)) {
+    throw std::runtime_error(
+      sb()  << "Error constructing regular square mesh, !(d > 0); "
+            << "d == " << d
+    );
+  }
+  return std::ceil((t1-t0)/d);
+}
+
+double 
+MeshRegularRectangular::impl_dx() const
+{
+  return dx_;
+}
+
+double 
+MeshRegularRectangular::impl_dy() const
+{
+  return dy_;
+}
--- contact-modelling/src/MeshRegularSquare.cpp
+++ contact-modelling/src/MeshRegularSquare.cpp
@@ -1,35 +1,12 @@
 #include "MeshRegularSquare.hpp"
 
-#include <stdexcept>
-#include <cmath>
-
-#include "helpers/string.hpp"
-
-using helpers::string::sb;
-
 MeshRegularSquare::MeshRegularSquare(const MeshInterface& base_mesh, const double d)
-  : MeshRegularSquare(
-      base_mesh.D,
-      base_mesh.minX(), 
-      base_mesh.minY(),
-      base_mesh.maxX(),
-      base_mesh.maxY(),
-      d
-    )
-{
-}
+  : MeshRegularRectangular(base_mesh, d, d)
+{}
 
 MeshRegularSquare::MeshRegularSquare(const MeshInterface& base_mesh, const double d, const size_t dim)
-  : MeshRegularSquare(
-      dim,
-      base_mesh.minX(), 
-      base_mesh.minY(),
-      base_mesh.maxX(),
-      base_mesh.maxY(),
-      d
-    )
-{
-}
+  : MeshRegularRectangular(base_mesh, d, d, dim)
+{}
 
 MeshRegularSquare::MeshRegularSquare(
   const size_t dim,
@@ -39,82 +16,5 @@ MeshRegularSquare::MeshRegularSquare(
   const double y1,
   const double d
 )
-  : MeshInterface(dim, calculate_no_nodes(x0, y0, x1, y1, d)), d_(d)
-{
-  const size_t nx       = calculate_no_nodes_1D(x0, x1, d);
-  const double diff_x   = nx * d - (x1 - x0);
-  const double x_origin = x0 - diff_x / 2.;
-
-  const size_t ny       = calculate_no_nodes_1D(y0, y1, d);
-  const double diff_y   = ny * d - (y1 - y0);
-  const double y_origin = y0 - diff_y / 2.;
-
-  size_t in = 0;
-  double x = x_origin;
-  for (size_t ix = 0; ix < nx; ++ix) {
-    double y = y_origin;
-    for (size_t iy = 0; iy < ny; ++iy) {
-      this->node(in).x = x;
-      this->node(in).y = y;
-      y += d;
-      ++in;
-    }
-    x += d;
-  }
-  generateMinMax();
-}
-
-double 
-MeshRegularSquare::impl_node_area(size_t i) const
-{
-  return d_*d_;
-}
-
-size_t 
-MeshRegularSquare::calculate_no_nodes(double x0, double y0, double x1, double y1, double d)
-{
-  if (!(x1 > x0)) {
-    throw std::runtime_error(
-      sb()  << "Error constructing regular square mesh, !(x1 > x0); "
-            << "x1 == " << x1 << "x0 == " << x0
-    );
-  }
-  if (!(y1 > y0)) {
-    throw std::runtime_error(
-      sb()  << "Error constructing regular square mesh, !(y1 > y0); "
-            << "y1 == " << y1 << "y0 == " << y0
-    );
-  }
-
-  return calculate_no_nodes_1D(x0, x1, d) * calculate_no_nodes_1D(y0, y1, d);
-}
-
-size_t 
-MeshRegularSquare::calculate_no_nodes_1D(double t0, double t1, double d)
-{
-  if (!(t1 > t0)) {
-    throw std::runtime_error(
-      sb()  << "Error constructing regular square mesh, !(t1 > t0); "
-            << "t1 == " << t1 << "t0 == " << t0
-    );
-  }
-  if (!(d > 0)) {
-    throw std::runtime_error(
-      sb()  << "Error constructing regular square mesh, !(d > 0); "
-            << "d == " << d
-    );
-  }
-  return std::ceil((t1-t0)/d);
-}
-
-double 
-MeshRegularSquare::impl_dx() const
-{
-  return d_;
-}
-
-double 
-MeshRegularSquare::impl_dy() const
-{
-  return d_;
-}
+  : MeshRegularRectangular(dim, x0, y0, x1, y1, d, d)
+{}
